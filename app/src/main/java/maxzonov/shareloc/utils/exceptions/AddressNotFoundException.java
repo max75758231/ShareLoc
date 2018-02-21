71afa72f94f064ff127be41c974e748230947ad4
MaxZonov <max75758231@yandex.ru>
2018-02-21 17:51:30 +0300

    AddressNotFoundException has been added.

---
+++ app/src/main/java/maxzonov/shareloc/utils/exceptions/AddressNotFoundException.java
@@ -0,0 +1,8 @@
+package maxzonov.shareloc.utils.exceptions;
+
+public class AddressNotFoundException extends Exception {
+
+    public AddressNotFoundException(String message) {
+        super(message);
+    }
+}
