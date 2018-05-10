041eb28c8cd2e66ec1a856c754c61bad2116a8e7
MaxZonov <max75758231@yandex.ru>
2018-05-10 20:59:15 +0300

    About app screen has been added. License has been replaced inside it.

---
+++ app/src/main/java/maxzonov/shareloc/ui/about/AboutActivity.java
@@ -0,0 +1,33 @@
+package maxzonov.shareloc.ui.about;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import maxzonov.shareloc.R;
+import maxzonov.shareloc.ui.license.LicenseActivity;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.about_license_layout)
+    void onLicenseClick() {
+        startActivity(new Intent(AboutActivity.this, LicenseActivity.class));
+    }
+
+    @OnClick(R.id.about_github_layout)
+    void onGithubClick() {
+        Uri uri = Uri.parse("https://github.com/max75758231/ShareLoc");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+}
