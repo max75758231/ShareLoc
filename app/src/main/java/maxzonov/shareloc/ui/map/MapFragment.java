7784e3046163bcf42d4977ece9b05c65e4e7c2c4
MaxZonov <max75758231@yandex.ru>
2018-09-07 08:09:03 +0300

    MapFragment has been refactored.

--- app/src/main/java/maxzonov/shareloc/ui/map/MapFragment.java
+++ app/src/main/java/maxzonov/shareloc/ui/map/MapFragment.java
@@ -2,7 +2,6 @@ package maxzonov.shareloc.ui.map;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomSheetBehavior;
@@ -35,6 +34,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import butterknife.Unbinder;
 import maxzonov.shareloc.R;
 import maxzonov.shareloc.preferences.PreferencesHelper;
+import maxzonov.shareloc.utils.MapComponentsConfigurator;
 
 public class MapFragment extends MvpAppCompatFragment implements OnMapReadyCallback,
         GoogleMap.OnMarkerDragListener,
@@ -93,22 +93,25 @@ public class MapFragment extends MvpAppCompatFragment implements OnMapReadyCallb
 
         initSharedPreferences();
 
-        if (!latitude.equals(getString(R.string.prefs_latitude_default))
-                && !longitude.equals(getString(R.string.prefs_longitude_default))) {
+        if (!latitude.equals(getString(R.string.prefs_latitude_default)) && !longitude.equals(getString(R.string.prefs_longitude_default))) {
             mapPresenter.getAddress(activity, latitude, longitude);
         }
 
         view = inflater.inflate(R.layout.fragment_map, container, false);
         unbinder = ButterKnife.bind(this, view);
 
+        constructBottomSheet();
+
+        return view;
+    }
+
+    private void constructBottomSheet() {
         View bottomSheet = view.findViewById(R.id.map_bottom_sheet_layout);
         sheetBehavior = BottomSheetBehavior.from(bottomSheet);
         sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
         sheetBehavior.setPeekHeight(0);
 
         bottomSheetLayout.setBackgroundResource(android.R.color.transparent);
-
-        return view;
     }
 
     @Override
@@ -127,36 +130,12 @@ public class MapFragment extends MvpAppCompatFragment implements OnMapReadyCallb
     public void onMapReady(GoogleMap googleMap) {
         MapsInitializer.initialize(activity);
 
-        this.googleMap = googleMap;
+        MapComponentsConfigurator mapConfigurator
+                = new MapComponentsConfigurator(convertStringToDouble(latitude), convertStringToDouble(longitude));
 
-        googleMap.setOnMarkerDragListener(this);
-        googleMap.getUiSettings().setZoomControlsEnabled(true);
-        googleMap.getUiSettings().setCompassEnabled(true);
+        this.googleMap = mapConfigurator.buildMap(this, googleMap, address);
 
-        CameraPosition cameraPosition;
-
-        if (latitude.equals(getString(R.string.prefs_latitude_default))
-                && longitude.equals(getString(R.string.prefs_longitude_default))) {
-
-            cameraPosition = CameraPosition.builder()
-                    .target(new LatLng(makeDouble(latitude), makeDouble(longitude)))
-                    .zoom(0)
-                    .bearing(0)
-                    .tilt(45)
-                    .build();
-        } else {
-
-            cameraPosition = CameraPosition.builder()
-                    .target(new LatLng(makeDouble(latitude), makeDouble(longitude)))
-                    .zoom(15)
-                    .bearing(0)
-                    .tilt(45)
-                    .build();
-
-            googleMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(makeDouble(latitude), makeDouble(longitude)))
-                    .draggable(true).title(address));
-        }
+        CameraPosition cameraPosition = mapConfigurator.buildCameraPosition();
 
         googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
@@ -223,7 +202,6 @@ public class MapFragment extends MvpAppCompatFragment implements OnMapReadyCallb
     /**
      * Users can move the marker with onDrag methods
      */
-
     @Override
     public void onMarkerDragStart(Marker marker) {
         marker.hideInfoWindow();
@@ -248,9 +226,17 @@ public class MapFragment extends MvpAppCompatFragment implements OnMapReadyCallb
     void onBottomSheetClicked() {
         sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
 
+        writeCoordsToPrefs();
+        
+        navigateToLocationScreen();
+    }
+
+    private void writeCoordsToPrefs() {
         prefsHelperLatitude.writeToPrefs(getString(R.string.prefs_latitude_key), latitude);
         prefsHelperLatitude.writeToPrefs(getString(R.string.prefs_longitude_key), longitude);
-        
+    }
+
+    private void navigateToLocationScreen() {
         if (listener != null) {
             listener.onLocationChanged(latitude, longitude, address);
         } else {
@@ -260,17 +246,17 @@ public class MapFragment extends MvpAppCompatFragment implements OnMapReadyCallb
     }
 
     @Override
+    public void showAndRefreshAddressInMarker(String address, Marker marker) {
+        showAddressInMarker(address);
+        marker.setTitle(address);
+    }
+
+    @Override
     public void showAddressInMarker(String address) {
         this.address = address;
     }
 
-    @Override
-    public void showAndRefreshAddressInMarker(String address, Marker marker) {
-        this.address = address;
-        marker.setTitle(address);
-    }
-
-    private double makeDouble(String coordinate) {
+    private double convertStringToDouble(String coordinate) {
         return Double.parseDouble(coordinate);
     }
 }
---
+++ app/src/main/java/maxzonov/shareloc/utils/MapComponentsConfigurator.java
@@ -0,0 +1,73 @@
+package maxzonov.shareloc.utils;
+
+import android.app.Activity;
+import android.content.Context;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapComponentsConfigurator {
+
+    private Double latitude;
+    private Double longitude;
+    private CameraPosition cameraPosition;
+
+    public MapComponentsConfigurator(Double latitude, Double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+
+
+    }
+
+    public CameraPosition buildCameraPosition() {
+
+        if (areCoordsEmpty()) {
+            buildDefaultCameraPosition();
+        } else {
+            buildDeterminedCameraPosition();
+        }
+
+        return cameraPosition;
+    }
+
+    public GoogleMap buildMap(GoogleMap.OnMarkerDragListener listener, GoogleMap googleMap, String markerAddress) {
+
+        googleMap.setOnMarkerDragListener(listener);
+        googleMap.getUiSettings().setZoomControlsEnabled(true);
+        googleMap.getUiSettings().setCompassEnabled(true);
+
+        if (!areCoordsEmpty()) {
+            googleMap.addMarker(new MarkerOptions()
+                    .position(new LatLng(latitude, longitude))
+                    .draggable(true).title(markerAddress));
+        }
+
+        return googleMap;
+    }
+
+
+
+    private void buildDefaultCameraPosition() {
+        cameraPosition = CameraPosition.builder()
+                .target(new LatLng(latitude, longitude))
+                .zoom(0)
+                .bearing(0)
+                .tilt(45)
+                .build();
+    }
+
+    private void buildDeterminedCameraPosition() {
+        cameraPosition = CameraPosition.builder()
+                .target(new LatLng(latitude, longitude))
+                .zoom(15)
+                .bearing(0)
+                .tilt(45)
+                .build();
+    }
+
+    private boolean areCoordsEmpty() {
+        return latitude.equals("0.0") && longitude.equals("0.0");
+    }
+}
